41a2431e45766f5e4f27fb8ce41b4910c098e4d5
sangwoo-p <45841851+sangwoo-p@users.noreply.github.com>
2023-04-10 02:27:35 +0100

    Add files via upload

---
+++ testing_with_given_measurements/nonlinear_AE_QCP_K_1/ibmq_qem.sh
@@ -0,0 +1,1 @@
+python ../../../../main_regression.py   --exp_mode 'toy_with_input' --set_prediction_mode 'CP' --NC_mode 'distribution_aware' --num_total_available_data 100 --num_layers 5  --if_pqc_angle_encoding_dnn --angle_encoding_mode_for_with_input 'learned_nonlinear'   --used_device 'ibmq_quito_with_qem'
---
+++ testing_with_given_measurements/nonlinear_AE_naive/simulator.sh
@@ -0,0 +1,1 @@
+python ../../../../main_regression.py  --exp_mode 'toy_with_input' --set_prediction_mode 'naive_quan' --NC_mode 'None' --num_total_available_data 100 --num_layers 5  --if_pqc_angle_encoding_dnn --angle_encoding_mode_for_with_input 'learned_nonlinear'   --used_device 'ibmq_qasm_simulator'
